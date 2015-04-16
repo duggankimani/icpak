d6ca9b29c2e509c2b9a0ce281e2849f43a6462b8
Duggan Kimani <mdkimani@gmail.com>
2015-04-16 15:25:24 +0300

    Date Serializer

--- src/main/java/com/icpak/rest/models/membership/Education.java
+++ src/main/java/com/icpak/rest/models/membership/Education.java
@@ -18,9 +18,11 @@ import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlSeeAlso;
 import javax.xml.bind.annotation.XmlTransient;
 
+import org.codehaus.jackson.map.annotate.JsonSerialize;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 
+import com.icpak.rest.DateSerializer;
 import com.icpak.rest.models.base.PO;
 import com.icpak.rest.models.util.Attachment;
 import com.wordnik.swagger.annotations.ApiModel;
@@ -53,7 +55,10 @@ public class Education extends PO{
 	
 	@Column(length=2000)
 	private String institution;
+	
+	@JsonSerialize(using=DateSerializer.class)
 	private Date startDate;
+	@JsonSerialize(using=DateSerializer.class)
 	private Date dateCompleted;
 	private String examiningBody;
 	private String classOrDivision;
